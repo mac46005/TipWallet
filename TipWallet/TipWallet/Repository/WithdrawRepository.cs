ee5fe8d727c8a9253a42a936de07c46a4f1d063a
Marco Preciado <mac46005@gmail.com>
2021-03-02 20:26:06 -0600

    Created repo class for withdrawModel

--- TipWallet/TipWallet/Repository/WithdrawRepository.cs
+++ TipWallet/TipWallet/Repository/WithdrawRepository.cs
@@ -1,9 +1,11 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using TipWallet.Models;
+using Dapper;
 
 namespace TipWallet.Repository
 {
@@ -15,34 +17,54 @@ namespace TipWallet.Repository
         public event EventHandler<WithdrawModel> OnObjUpdated;
         public event EventHandler<WithdrawModel> OnObjDeleted;
 
-        private async  Task CreateConnection()
+        private async Task CreateConnection()
         {
+            if (_connection != null)
+            {
+                return;
+            }
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var databasePath = Path.Combine(documentPath, "TipWallet_DB.db");
+            _connection = new SQLiteAsyncConnection(databasePath);
+            await _connection.CreateTableAsync<WithdrawModel>();
+        }
+
+        public async Task AddItem(WithdrawModel obj)
+        {
+             await CreateConnection();
+            await _connection.InsertAsync(obj);
 
         }
 
-        public Task AddItem(WithdrawModel obj)
+        public async Task AddOrUpdate(WithdrawModel obj)
         {
-            throw new NotImplementedException();
+            await CreateConnection();
+            if (obj.Id == 0)
+            {
+                await AddItem(obj);
+            }
+            else
+            {
+                await UpdateItem(obj);
+            }
         }
 
-        public Task AddOrUpdate(WithdrawModel obj)
+        public async Task DeleteItem(WithdrawModel obj)
         {
-            throw new NotImplementedException();
+            await CreateConnection();
+            await _connection.DeleteAsync(obj);
         }
 
-        public Task DeleteItem(WithdrawModel obj)
+        public async Task<List<WithdrawModel>> GetItems()
         {
-            throw new NotImplementedException();
+            await CreateConnection();
+            return await _connection.Table<WithdrawModel>().ToListAsync();
         }
 
-        public Task<List<WithdrawModel>> GetItems()
+        public async Task UpdateItem(WithdrawModel obj)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateItem(WithdrawModel obj)
-        {
-            throw new NotImplementedException();
+            await CreateConnection();
+            await _connection.UpdateAsync(obj);
         }
     }
 }
