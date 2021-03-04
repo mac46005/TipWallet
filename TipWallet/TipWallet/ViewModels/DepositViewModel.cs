a65fc7d450892ca02a35953b7f0618ab86aca18c
Marco Preciado <mac46005@gmail.com>
2021-03-04 14:06:59 -0600

    Workinf on Deposit View/ViewModel

--- TipWallet/TipWallet/ViewModels/DepositViewModel.cs
+++ TipWallet/TipWallet/ViewModels/DepositViewModel.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using TipWallet.Repository;
+using Xamarin.Forms;
 
 namespace TipWallet.ViewModels
 {
     public class DepositViewModel : ViewModel
     {
+        DepositRepository _depoRepo;
+        public DepositViewModel(DepositRepository depoRepo)
+        {
+            _depoRepo = depoRepo;
+        }
+
+        ///////////////Buttons///////////////
+
+        /// <summary>
+        /// This will add the funds to the depoRepo table
+        /// </summary>
+        public ICommand AddFunds => new Command(async () =>
+        {
+
+        });
+
     }
 }
