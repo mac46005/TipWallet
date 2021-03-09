c585204eda58e7bf24e70bc1c03e499f8cc83d91
Marco Preciado <mac46005@gmail.com>
2021-03-08 20:07:45 -0600

    Testing Withdraw view/viewmodel

--- TipWallet/TipWallet/ViewModels/WithdrawViewModel.cs
+++ TipWallet/TipWallet/ViewModels/WithdrawViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using TipWallet.Models;
+using TipWallet.Repository;
+using Xamarin.Forms;
 
 namespace TipWallet.ViewModels
 {
     public class WithdrawViewModel : ViewModel
     {
+        WithdrawRepository _withdrawRepo;
+        public WithdrawViewModel(WithdrawRepository withdrawRepo)
+        {
+            _withdrawRepo = withdrawRepo;
+        }
+        public WithdrawModel WithdrawModel { get; set; }
+
+        public ICommand SubmitButton => new Command(async () =>
+        {
+            await _withdrawRepo.AddItem(WithdrawModel);
+            await Navigation.PopAsync();
+        });
     }
 }
